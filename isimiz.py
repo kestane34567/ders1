7678e3e6d5bf909c4581ae10d3d1d0aff59981b8
kestane34567 <164732551+kestane34567@users.noreply.github.com>
2024-04-18 21:31:33 +0300

    Create isimiz.py

---
+++ isimiz.py
@@ -0,0 +1,17 @@
+meme_dict = {
+            "CRINGE": "Garip ya da utandırıcı bir şey",
+            "LOL": "Komik bir şeye verilen cevap",
+            "IDK" : "Bilmiyorum"
+            }
+            
+
+word = input("Anlamadığınız bir kelime yazın (hepsini büyük harflerle yazın!): ")
+
+
+if word in meme_dict.keys():
+    # Kelime eşleşiyorsa ne yapmalıyız?
+    print(meme_dict[word])
+    
+else:
+    # Kelime eşleşmiyorsa ne yapmalıyız?
+    print("Kelime yok..")
